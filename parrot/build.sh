884b94419bd11b796c985cb2a0058302768e1edb
tonyarris <ash552@york.ac.uk>
2022-02-11 17:41:22 +0100

    Fix typo

--- parrot/build.sh
+++ parrot/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # builds the container - to run after a change to the dockerfile manifest and on a regular basis to keep the container updated
-sudo docker build --no-cache -t kali .
+sudo docker build --no-cache -t parrot .
