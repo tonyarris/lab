47eea6dca770678a3edd1fbe3a49a3b572c52dae
tonyarris <ash552@york.ac.uk>
2022-02-12 14:25:58 +0100

    Change permissions on scripts and update terraform manifest

--- terraform/README.md
+++ terraform/README.md
@@ -1,9 +1,20 @@
 AWS EC2 Provisioning with Terraform
 ===
 
-Requirements:
+# Requirements:
 - [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html)
 - [Terraform CLI](https://learn.hashicorp.com/tutorials/terraform/install-cli)
 
 [Tutorial](https://learn.hashicorp.com/tutorials/terraform/aws-build)
+
+# Summary
+Ensure you have `terraform` and `aws` CLI installed, and add IAM an Access key to AWS with `aws configure`
+ 
+Edit the contents of `main.tf` to suit your needs.
+
+From within this folder, initiate terraform with `terraform init`
+
+Provision the instance with `terraform apply`
+
+Destroy the instance with `terraform destroy`
 
--- terraform/main.tf
+++ terraform/main.tf
@@ -15,10 +15,47 @@ provider "aws" {
 }
 
 resource "aws_instance" "pt_lab" {
-  ami           = "ami-0257e70b7c6db1498"
+  ami           = "ami-0f8ce9c417115413d"
   instance_type = "t3.micro"
+  associate_public_ip_address = true
+  key_name         = "ssh-key"
 
   tags = {
     Name = "PT Lab Instance"
   }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    # clone lab repo and setup filesystem
+    cd /home/$(whoami)
+    git clone https://github.com/tonyarris/lab
+    cd lab
+    cp stop.sh kali
+    cp stop.sh parrot
+    rm stop.sh
+    rm -r terraform/
+
+    # install docker
+    sudo apt-get -y update
+    sudo apt-get -y install docker.io
+
+    # build containers
+    cd kali
+    sudo chmod +x *.sh
+    sudo ./build.sh
+    cd ../parrot
+    sudo chmod +x *.sh
+    sudo ./build.sh
+    
+  EOF
+}
+
+output "instance_ip" {
+  description = "The public ip for ssh access"
+  value       = aws_instance.pt_lab.public_ip
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name   = "ssh-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCl9VKriojo6Q60esciAHQxKh8g6Tb00eYIHdAm8RuqPry5xTFP+gh0GS4ub0hZBnOyvC6sa4B/1MTNjL+Ex4rkUfM0gHH0PSSImJPBzKtQQ+sCxk4TsKTjfqxUHQQeaQgqufpy9uiXLRKdX0mZJLWv/Q2o6Dbcj7XhZqdrSzLKHqm7cCjv5b7AzVCRXlENA7fFSDXuupaNyWwfubn55qVielLQOJOTnNuuS2RQrNKDVWSMqAVds+E0SeFVikRAqhbd0YNWYqs139Z/bu9R9vMhLs1HElRimCwx1itjn+GHCLQ25chJHu1+snJmWakjxuLQz0Y8qoo+xrFluzQXQWo1"
 }
